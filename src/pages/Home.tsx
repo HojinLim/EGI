0c441ccfe07f7e91b47e55a97b835359a760bcc3
SEONGROK <129926477+pigrok@users.noreply.github.com>
2023-08-11 13:14:39 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,7 @@ import GetPost from '../components/posts/GetPost';
 const Home = () => {
   return (
     <div>
-      <div style={{ maxWidth: '1200px', margin: '0 auto' }} />
-      <div>
-        <GetPost />
-      </div>
+      <GetPost />
     </div>
   );
 };
