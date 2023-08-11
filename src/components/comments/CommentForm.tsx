8d810746fb15ba1f24fed2e147c97d59ed207354
HojinLim <hojinim@gmail.com>
2023-08-11 18:07:08 +0900

    Feature/에러 팝업

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,6 +1,8 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React, { Dispatch, SetStateAction, useState, useEffect } from 'react';
 import useCommentMutation from '../../hooks/useCommentMutation';
 import * as S from './Styled.Comments';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 interface CommentFormProps {
   uid: string;
@@ -9,33 +11,56 @@ interface CommentFormProps {
 }
 
 const CommentForm = ({ uid, pid, setIsCommenting }: CommentFormProps) => {
+  const [showAlert, setShowAlert] = useState(false);
+
   const { addCommentMutation } = useCommentMutation();
   const [commentText, setCommentText] = useState('');
+
   const handleCommentInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCommentText(e.target.value);
   };
+
   const handleAddSubmitBtn = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (commentText === '') {
-      alert('댓글을 작성해 주세요.');
-      return false;
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+
+      const newComment = {
+        uid: uid,
+        pid: Number(pid),
+        nickname: '테스트',
+        body: commentText,
+        created_at: new Date().toISOString()
+      };
+
+      addCommentMutation.mutate(newComment);
+      setCommentText('');
+      setIsCommenting(false);
     }
-
-    const newComment = {
-      uid: uid,
-      pid: Number(pid),
-      nickname: '테스트',
-      body: commentText,
-      created_at: new Date().toISOString()
-    };
-
-    addCommentMutation.mutate(newComment);
-    setCommentText('');
-    setIsCommenting(false);
   };
+
+  // 3초후 오류 팝업 사라짐
+  useEffect(() => {
+    if (showAlert) {
+      const timeoutId = setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [showAlert]);
+
   return (
     <S.CommentForm onSubmit={handleAddSubmitBtn}>
+      {showAlert && (
+        // MUI Alert기능
+        <Stack sx={{ width: '100%', position: 'fixed', top: 0, zIndex: 100 }}>
+          <Alert severity="error">댓글을 작성해 주세요.</Alert>
+        </Stack>
+      )}
       <S.CommentItem>
         <S.CommentProfileImgBox>사진</S.CommentProfileImgBox>
         <S.CommentInput type="text" value={commentText} onChange={handleCommentInputChange} />
